99400c413acbb0e5c1280a25593eb326ad037b32
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-01 11:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function indexOfIgnoreCase(str, subStr) {
 }
 
 // Please do not change the code below
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(indexOfIgnoreCase(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
